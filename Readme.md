ec78bfed1359366691ed9fc840d1264742d3a64f
skdding3 <skdding3@gmail.com>
2022-04-21 02:33:18 +0900

    Beta

--- Readme.md
+++ Readme.md
@@ -3,7 +3,7 @@
 ### SKILL : 
 <img src="https://img.shields.io/badge/Html5-E44D26?style=flat-square&logo=Html5&logoColor=white"/> <img src="https://img.shields.io/badge/CSS3-1572B6?style=flat-square&logo=CSS3&logoColor=white"/> <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/>
 
-원초적인 기본기 단련을 위해 프레임워크는 별도로 사용하지 않았습니다.
+원초적인 기본기 단련을 위해 프레임워크는 별도로 사용하지 않았습니다. (Vanilla JS)
 
 --------
 
--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ searchInputEl.addEventListener('blur', function () {
 
 // badges
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 // scroll event
 window.addEventListener('scroll', _.throttle(function () {
@@ -28,16 +29,30 @@ window.addEventListener('scroll', _.throttle(function () {
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
-    })
+    });
+    // 버튼 보이기
+    gsap.to('#to-top', .2, {
+      x: 0
+    });
   } else {
     // 배지 보이기
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
-    })
+    });
+    // 버튼 숨기기
+    gsap.to('#to-top', .2, {
+      x: 100
+    });
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+});
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
@@ -70,6 +85,16 @@ new Swiper('.promotion .swiper-container', {
   navigation: {
     prevEl: '.promotion .swiper-prev',
     nextEl: '.promotion .swiper-next'
+  }
+});
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
   }
 });
 
@@ -112,3 +137,19 @@ function floatingObject(selector,delay,size) {
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: .8
+  })
+  .setClassToggle(spyEl, 'show')
+  .addTo(new ScrollMagic.Controller());
+});
+
+// 년도 날짜 자동함수
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // 2022
